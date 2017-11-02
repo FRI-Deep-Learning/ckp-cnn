bdf424a7ed0b8df9abb9b2c112fb4218b83686ee
Eli Dworetzky <eli98612@gmail.com>
2017-11-02 17:13:00 -0400

    Update raw_to_png.py

--- raw_to_png.py
+++ raw_to_png.py
@@ -1,12 +1,17 @@
 from PIL import Image, ImageFilter
+import os
 
-def convert(imgName,resizeX,resizeY):
-    rawImg = open(imgName,'rb').read()
+path = 'C:/Users/IASA-FRI/Desktop/arfacetest/ARFaceDatabase/temp'
+dest = 'C:/Users/IASA-FRI/Desktop/arfacetest/ARFaceDatabase/arface_png'
+
+def rawToPng(imgDir,imgName,resizeX,resizeY,destDir):
+    rawImg = open(imgDir + '/' + imgName,'rb').read()
     imgSize = (256,577)
     imgSize = (256,570)
     newImg = Image.frombytes('RGB',imgSize,rawImg,'raw')
     newImg = newImg.resize((resizeX,resizeY), Image.ANTIALIAS)
-    newImg.save(img[:len(img)-4]+'.png')
+    newImg.save(dest + '/' + imgName[:len(img)-4]+'.png')
 
-img = 'test1.raw'
-convert(img,96,96)
+images = os.listdir(path)
+for img in images:
+    rawToPng(path,img,96,96,dest)
